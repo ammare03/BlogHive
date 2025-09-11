48514c1edf59d7f84b78531cddc93c74eeb4d04e
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-09-11 11:09:40 +0530

    feat: implement API Gateway with Eureka server configuration and Maven wrapper

--- MS1/src/main/java/com/example/a013/bloghive/BlogHiveApplication.java
+++ MS1/src/main/java/com/example/a013/bloghive/BlogHiveApplication.java
@@ -2,8 +2,10 @@ package com.example.a013.bloghive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BlogHiveApplication {
 
     public static void main(String[] args) {
