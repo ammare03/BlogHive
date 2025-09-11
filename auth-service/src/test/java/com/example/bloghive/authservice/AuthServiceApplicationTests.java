652d2ac8477daa959894440826dfe6e98e42a6df
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-09-11 11:14:51 +0530

    feat: add initial auth-service structure with Maven configuration and application files

---
+++ auth-service/src/test/java/com/example/bloghive/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloghive.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
