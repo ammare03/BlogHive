98bec8571837eca6141e83ff08026e48e5cfe741
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-09-11 10:16:54 +0530

    Initial commit

---
+++ src/main/java/com/example/a013/bloghive/BlogHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.a013.bloghive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogHiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/a013/bloghive/BlogHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a013.bloghive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
