28f78bde20329c228789b451c05fcdd6b7ceee56
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-09-14 11:24:41 +0530

    feat: initialize eureka-server project with Maven setup and basic application structure

---
+++ eureka-server/src/test/java/com/bloghive/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloghive.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
