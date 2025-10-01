73abc2e98d0d1d62c3eb37449c47bcaf3ec88fcb
Vistaar <vistaar007@gmail.com>
2025-10-01 11:35:44 +0530

    feat: Initialize notification service with Maven setup and basic application structure

---
+++ notification-service/src/test/java/com/bloghive/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloghive.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
